2c5adff8e065bdadb4165e4401d847179ad9636b
Robert Wikman <rbw@vault13.org>
2018-03-10 00:14:29 +0100

    Use vanilla route for routes EP

--- examples/full/app/api/_routes/routes.py
+++ examples/full/app/api/_routes/routes.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint, jsonify
-from flask_journey.utils import route
 from app.core import journey
 
 bp = Blueprint('routes', __name__)
 
 
-@route(bp, '/', methods=['GET'])
+@bp.route('/', methods=['GET'])
 def get_many():
     return jsonify(journey.routes_detailed)
 
