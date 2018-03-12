fd646b855b81caa92c45d05ed6ea25bdae978d6a
Robert Wikman <rbw@vault13.org>
2018-03-12 16:09:48 +0100

    moved routes to server-info bundle

--- examples/full/app/api/__init__.py
+++ examples/full/app/api/__init__.py
@@ -3,13 +3,14 @@
 from app import factory
 from app.core import journey
 
-from .bundles import v1
+from .bundles import v1, server_info
 
 
 def create_app():
     app = factory.create_app(__name__)
 
     journey.attach_bundle(v1)
+    journey.attach_bundle(server_info)
     journey.init_app(app)
 
     return app
--- examples/full/app/api/bundles.py
+++ examples/full/app/api/bundles.py
@@ -6,8 +6,11 @@ from .planes import bp as planes
 from .pilots import bp as pilots
 from .routes import bp as routes
 
+server_info = BlueprintBundle(path='/server-info')
+server_info.attach_bp(routes)
+
 v1 = BlueprintBundle(path='/api/v1')
 v1.attach_bp(planes, description='Planes API, CRUD')
 v1.attach_bp(pilots, description='Info about pilots')
-v1.attach_bp(routes)
+
 
